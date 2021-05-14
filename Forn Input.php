afbea5db76f0c945b7c20478c9a4c35573756ffc
Dhea Sagita <81453793+dheasagita@users.noreply.github.com>
2021-05-14 22:29:28 +0700

    Add files via upload

---
+++ Forn Input.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+<body>
+    <h1>Form Input</h1>
+    <form method="post">
+        <label>Nama: </label>
+        <input type="text" name="nama">
+        <input type="submit" value="Kirim">
+    </form>
+    <?php
+    echo 'Selamat Datang ' . $_POST['nama'];
+    ?>
+</body>
+</html>
+
